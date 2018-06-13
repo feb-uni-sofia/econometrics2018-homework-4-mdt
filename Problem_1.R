989f7fededab6e1cb9169f174a6dc2740fff668a
tanyamll <37013663+tanyamll@users.noreply.github.com>
2018-06-13 21:12:50 +0300

    Add files via upload

--- Problem_1.R
+++ Problem_1.R
@@ -7,21 +7,51 @@ houseWork <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/da
 str(houseWork)
 
 ## a)
+table (houseWork$sex)
 
 ## b)
 
+Hworkf <- houseWork$hours[houseWork$sex == 'f']
+Hworkm <- houseWork$hours[houseWork$sex == 'm']
+
+avgworkF <- mean(Hworkf)
+avgworkM <- mean(Hworkm)
+
 ## c)
+
+houseWork <- within(houseWork, {
+  female <- ifelse(sex == 'f', TRUE , FALSE)
+  male <- ifelse(sex == 'm', TRUE, FALSE)
+})
+
+str(houseWork)
 
 ## d)
 
+fit <- lm ( hours ~ female , data = houseWork)
+summary(fit)
+
+
 ## e)
 
+
+
 ## f)
+### H0 : The average house-work hours for women are less than or equal to the average house-work hours for men 
+### H1 : The average house-work hours for women are greater than the average house-work hours for men
 
 ## g)
 
+T <- (-14.4555 - 32.8138) /0.3186
+pt ( -T , df = 2)
+
 ## h)
+
+##we cannot reject H0: µf ≤ µm at a 1% error probability, because the test-statistic is less than the crittical value.
 
 ## i)
 
 ## j)
+
+fit1 <- lm( hours ~ female + male , data=houseWork) 
+summary(fit1)
--- Problem_2.R
+++ Problem_2.R
@@ -4,10 +4,36 @@ str(crime)
 
 ## a)
 
+fit <- lm (C ~ HS, data = crime)
+summary(fit)
+
+## There is positiv elasticity for HS that mean that when we observe promotion at HS
+##there is also promotion at Crime rate, but that doesn't mean that they are related 
+
 ## b)
 
+plot(crime[2:5])
+
 ## c)
+summary(crime)
+fit1 <- lm( C ~ HS + U, data = crime)
+summary(fit)
+
+###We would explain that the estimated coefficient 
+##of HS changes its sign when U is included in the model with that HS is dependet from U and influence it. It 
+## can be that the people who living in the urban environment are more likely to study in HS than the others.
 
 ## d)
 
+##  Betweeen the education and crime rates is no direct connection, cuz we can't say that the reduce of 
+## education spending can cause the low levels of crime or the opposite. Also there are many factors 
+##that influence the rate
+
 ## e)
+
+fit1 <- lm( C ~ HS + U + I, data = crime)
+summary(fit1)
+
+
+
+
